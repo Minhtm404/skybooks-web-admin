a7b2b1e29c4489e15ea841a77222cd49ba58321f
Minh Tam <caominhtam.10a4@gmail.com>
2023-09-11 18:13:36 +0700

    fix layout

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ const App = () => {
             <TooltipComponent content="Settings" position="Top">
               <button
                 type="button"
-                className="text-3xl rounded-full p-3 hover:bg-light-gray "
+                className="text-3xl text-white p-3 hover:drop-shadow-xl hover:bg-light-gray"
                 style={{ background: 'blue', borderRadius: '50%' }}
               >
                 <FiSettings />
@@ -53,7 +53,7 @@ const App = () => {
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
               <Sidebar />
             </div>
           ) : (
@@ -61,39 +61,39 @@ const App = () => {
               <Sidebar />
             </div>
           )}
-          
           <div
             className={
               activeMenu
-                ? 'dark:bg-main-dark-bg  bg-main-bg min-h-screen md:ml-72 w-full'
+                ? 'dark:bg-main-dark-bg bg-main-bg min-h-screen md:ml-72 w-full'
                 : 'bg-main-bg dark:bg-main-dark-bg  w-full min-h-screen flex-2'
             }
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
-          </div>
 
-          <div>
-            <Routes>
-              <Route path="/" element={<Ecommerce />}></Route>
-              <Route path="/ecommerce" element={<Ecommerce />}></Route>
+            <div>
+              <Routes>
+                <Route path="/" element={<Ecommerce />} />
+                <Route path="/ecommerce" element={<Ecommerce />} />
 
-              <Route path="/employees" element={<Employees />}></Route>
-              <Route path="/collections" element={<Collections />}></Route>
-              <Route path="/products" element={<Products />}></Route>
-              <Route path="/customers" element={<Customers />}></Route>
-              <Route path="/orders" element={<Orders />}></Route>
+                <Route path="/employees" element={<Employees />} />
+                <Route path="/collections" element={<Collections />} />
+                <Route path="/products" element={<Products />} />
+                <Route path="/customers" element={<Customers />} />
+                <Route path="/orders" element={<Orders />} />
 
-              <Route path="/line" element={<Line />}></Route>
-              <Route path="/area" element={<Area />}></Route>
-              <Route path="/bar" element={<Bar />}></Route>
-              <Route path="/pie" element={<Pie />}></Route>
-              <Route path="/financial" element={<Financial />}></Route>
-              <Route path="/color-mapping" element={<ColorMapping />}></Route>
-              <Route path="/pyramid" element={<Pyramid />}></Route>
-              <Route path="/stacked" element={<Stacked />}></Route>
-            </Routes>
+                <Route path="/line" element={<Line />} />
+                <Route path="/area" element={<Area />} />
+                <Route path="/bar" element={<Bar />} />
+                <Route path="/pie" element={<Pie />} />
+                <Route path="/financial" element={<Financial />} />
+                <Route path="/color-mapping" element={<ColorMapping />} />
+                <Route path="/pyramid" element={<Pyramid />} />
+                <Route path="/stacked" element={<Stacked />} />
+              </Routes>
+            </div>
+            <Footer />
           </div>
         </div>
       </BrowserRouter>
