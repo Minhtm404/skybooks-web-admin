4108db7d78c03cb388869675ad9b897bd2452dba
Minh Tam <caominhtam.10a4@gmail.com>
2023-11-14 00:45:35 +0700

    add user profile

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,5 +1,5 @@
-import React, { useContext } from 'react';
-import { Spinner } from 'flowbite-react';
+import React, { useContext, useState } from 'react';
+import { Button, Label, Spinner, TextInput } from 'flowbite-react';
 
 import { Context as AuthContext } from '../contexts/AuthContext';
 
@@ -7,6 +7,9 @@ import { Header } from '../components';
 
 const UserProfile = () => {
   const { user, isLoading } = useContext(AuthContext);
+
+  const [name, setName] = useState(user.name);
+  const [email, setEmail] = useState(user.email);
 
   if (isLoading) {
     return (
@@ -20,6 +23,39 @@ const UserProfile = () => {
     return (
       <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl dark:bg-gray-800 dark:border-gray-700">
         <Header title="User Profile" />
+
+        <div>
+          <form className="flex max-w-md flex-col gap-4">
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="name" value="Your name" />
+              </div>
+              <TextInput
+                id="name"
+                type="text"
+                placeholder="Enter your name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <div className="mb-2 block">
+                <Label htmlFor="email" value="Your email" />
+              </div>
+              <TextInput
+                id="email"
+                type="email"
+                placeholder="Enter your email"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                required
+              />
+            </div>
+
+            <Button type="submit">Update data</Button>
+          </form>
+        </div>
       </div>
     );
   }
